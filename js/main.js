9111b489f92b60029a8fe4dcc8774487cd8078f2
Eniola Johnson <writingincodes@gmail.com>
2020-12-18 01:14:53 +0100

    User can select an answer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var $username = document.querySelector('h1');
 var $text = document.querySelector('h3');
+var $form = document.querySelector('#form');
 var $inputName = document.querySelector('#input-name');
-var $submitButton = document.querySelector('#submit-button');
-
+var $enterButton = document.querySelector('#enter-button');
+var $startButton = document.querySelector('#start-button');
+var $buttons = document.querySelector('.buttons');
 
 function changeName(e) {
   if($inputName.value <= 0){
@@ -10,9 +12,66 @@ function changeName(e) {
   } else {
     $username.textContent = $inputName.value + ',';
     $text.textContent = 'ready to test your knowledge on earthly things?';
-    $submitButton.value = 'Start Now';
-    $inputName.style.display = 'none';
+    $inputName.classList.add('hidden');
+    $enterButton.classList.add('hidden');
+    $startButton.classList.remove('hidden');
+    userData.username = $inputName.value;
   }
 }
 
-$submitButton.addEventListener('click', changeName);
+$enterButton.addEventListener('click', changeName);
+
+function startGame(e){
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
+
+  xhr.responseType = 'json';
+
+  xhr.addEventListener('load', function () {
+  var xhrResponse = xhr.response;
+  var data = xhrResponse.results;
+  var answers = [];
+  var correctAnswer = '';
+  var score = 0;
+
+    for (var i = 0; i < data.length; i++) {
+      // console.log(data[i].question);
+      $text.textContent = data[i].question;
+      $startButton.classList.add('hidden');
+      correctAnswer = data[i].correct_answer;
+      answers = (data[i].incorrect_answers);
+      answers.push(correctAnswer);
+      console.log(correctAnswer);
+      }
+
+    answers.sort();
+
+
+    for (answer in answers){
+      var $answerButton = document.createElement('button');
+      $answerButton.textContent = answers[answer];
+      $buttons.append($answerButton);
+      $answerButton.addEventListener("click", function(e){
+        if(e.target.textContent === correctAnswer){
+          e.target.style.backgroundColor = 'green';
+          score++;
+        } else {
+          e.target.style.backgroundColor = 'red';
+        }
+      })
+    }
+})
+
+xhr.send();
+}
+
+
+
+$startButton.addEventListener('click', startGame);
+
+function quizQuestion(){
+  $text.textContent = data[i].question;
+  $username.textContent = '';
+  $inputName.style.display = 'none';
+}
