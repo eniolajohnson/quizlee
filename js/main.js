73734dcb0ccc724b8fa7d20e04aca6205c86e5f0
Eniola Johnson <writingincodes@gmail.com>
2021-01-04 19:01:44 +0100

    reverted to the old code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-const $username = document.querySelector('h1');
-const $text = document.querySelector('h3');
-const $form = document.querySelector('#form');
-const $inputName = document.querySelector('#input-name');
-const $enterButton = document.querySelector('#enter-button');
-const $startButton = document.querySelector('#start-button');
-const $buttons = document.querySelectorAll('button');
-const $answerButtons = document.querySelector('.buttons');
-const $p = document.querySelector('p');
-const $score = document.querySelector('span');
-const $body = document.querySelector('div');
-const $main = document.querySelector('main');
+var $username = document.querySelector('h1');
+var $text = document.querySelector('h3');
+var $form = document.querySelector('#form');
+var $inputName = document.querySelector('#input-name');
+var $enterButton = document.querySelector('#enter-button');
+var $startButton = document.querySelector('#start-button');
+var $buttons = document.querySelectorAll('button');
+var $answerButtons = document.querySelector('.buttons');
+var $p = document.querySelector('p');
+var $score = document.querySelector('span');
+var $body = document.querySelector('div');
+var $main = document.querySelector('main');
 
-const changeName = (e) => {
+function changeName(e) {
   if($inputName.value <= 0){
-    $username.textContent = `We need a name to play the game...`;
+    $username.textContent = 'We need a name to play the game...';
   } else {
-    $username.textContent = `${$inputName.value} ,`;
-    $text.textContent = `ready to test your knowledge on earthly things?`;
+    $username.textContent = $inputName.value + ',';
+    $text.textContent = 'ready to test your knowledge on earthly things?';
     $inputName.classList.add('hidden');
     $enterButton.classList.add('hidden');
     $startButton.classList.remove('hidden');
@@ -26,14 +26,14 @@ const changeName = (e) => {
 
 $enterButton.addEventListener('click', changeName);
 
-let count = 11;
-let score = 0;
-const $restart = document.createElement('button');
+var count = 11;
+var score = 0;
+var $restart = document.createElement('button');
 $restart.className = 'play-again';
 $restart.textContent = 'Play Again';
 
-const startGame = (e) => {
-  let xhr = new XMLHttpRequest();
+function startGame(e){
+  var xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple');
 
@@ -41,23 +41,23 @@ const startGame = (e) => {
 
   count--;
 
-  xhr.addEventListener('load', () => {
-  let xhrResponse = xhr.response;
-  let data = xhrResponse.results;
-  let answers = [];
-  let unfilteredCorrectAnswer = '';
+  xhr.addEventListener('load', function(e) {
+    var xhrResponse = xhr.response;
+    var data = xhrResponse.results;
+    var answers = [];
+    var unfilteredCorrectAnswer = '';
 
-    for (let i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
       $text.innerHTML = data[i].question;
       $startButton.value = 'Next';
       unfilteredCorrectAnswer = data[i].correct_answer;
-      const filteredCorrectAnswer = document.createElement('p');
+      var filteredCorrectAnswer = document.createElement('p');
       filteredCorrectAnswer.innerHTML = unfilteredCorrectAnswer;
-      const correctAnswer = filteredCorrectAnswer.textContent;
+      var correctAnswer = filteredCorrectAnswer.textContent;
       answers = (data[i].incorrect_answers);
       answers.push(correctAnswer);
       answers.sort();
-      $buttons.forEach((elem) => {
+      $buttons.forEach(function(elem) {
         elem.classList.remove('hidden');
       })
       $p.classList.remove('hidden');
@@ -66,15 +66,15 @@ const startGame = (e) => {
       $buttons[2].innerHTML = answers[2];
       $buttons[3].innerHTML = answers[3];
 
-      e.target.addEventListener('click', (e) => {
-        $buttons.forEach((elem) => {
+      e.target.addEventListener('click', function(e) {
+        $buttons.forEach(function(elem) {
           if (elem.textContent === correctAnswer){
             elem.className = 'green';
           }
         })
       })
-      $buttons.forEach((elem) => {
-        elem.addEventListener("click", (e) => {
+      $buttons.forEach(function(elem) {
+        elem.addEventListener("click", function(e) {
           if (e.target.textContent === correctAnswer) {
             score++;
             e.target.className = 'green';
@@ -91,7 +91,7 @@ const startGame = (e) => {
       $score.className = 'score';
       $score.textContent = `Score: ${score}`;
       $p.textContent = '';
-      $buttons.forEach((elem) => {
+      $buttons.forEach(function(elem) {
         elem.className ='gray';
       })
 
@@ -99,7 +99,7 @@ const startGame = (e) => {
 
     if (count < 1){
       $username.textContent = `Welldone, ${$inputName.value}!`;
-      $buttons.forEach((elem) => {
+      $buttons.forEach(function(elem) {
         elem.classList.add('hidden');
       })
       $startButton.classList.add('hidden');
@@ -110,7 +110,7 @@ const startGame = (e) => {
       } else if (score > 7){
         $text.textContent = `We are in awe of your knowledge!`;
       }
-      const total = 10;
+      var total = 10;
       $p.textContent = `You got ${score} of ${total} correctly.`;
       $score.className = 'hidden';
       $body.append($restart);
@@ -120,60 +120,60 @@ const startGame = (e) => {
 xhr.send();
 }
 
-let counter = 11;
-let scorer = 0;
+var counter = 11;
+var scorer = 0;
 
-$restart.addEventListener('click', (e) => {
+$restart.addEventListener('click', function(e) {
   counter = 10;
   scorer = 0;
   while ($main.firstChild) {
     $main.removeChild($main.firstChild);
   }
-  const container = document.createElement('div');
+  var container = document.createElement('div');
   container.className = 'container';
 
-  const h1 = document.createElement('h1');
+  var h1 = document.createElement('h1');
   h1.textContent = userData.username + ',';
 
-  const span = document.createElement('span');
+  var span = document.createElement('span');
   span.className = 'hidden';
   span.textContent = 'Score: 0';
 
-  const h3 = document.createElement('h3');
+  var h3 = document.createElement('h3');
   h3.textContent = `ready to test your knowledge on earthly things?`;
 
-  const div1 = document.createElement('div');
-  const input1 = document.createElement("INPUT");
+  var div1 = document.createElement('div');
+  var input1 = document.createElement("INPUT");
   input1.className = 'hidden';
   input1.id = 'input-name';
   input1.setAttribute("type", "text");
   input1.setAttribute("placeholder", "Name goes here...");
   div1.append(input1);
 
-  const div2 = document.createElement('div');
-  const input2 = document.createElement("INPUT");
+  var div2 = document.createElement('div');
+  var input2 = document.createElement("INPUT");
   input2.id = 'enter-button';
   input2.className = 'hidden';
   input2.setAttribute("type", "submit");
   input2.setAttribute("value", "Enter...");
   div2.append(input2);
 
-  const div3 = document.createElement('div');
+  var div3 = document.createElement('div');
   div3.className = 'buttons';
-  const button1 = document.createElement('button');
+  var button1 = document.createElement('button');
   button1.className = 'hidden';
-  const button2 = document.createElement('button');
+  var button2 = document.createElement('button');
   button2.className = 'hidden';
-  const button3 = document.createElement('button');
+  var button3 = document.createElement('button');
   button3.className = 'hidden';
-  const button4 = document.createElement('button');
+  var button4 = document.createElement('button');
   button4.className = 'hidden';
-  const p = document.createElement('p');
+  var p = document.createElement('p');
   p.className = 'hidden';
   div3.append(button1, button2, button3, button4, p);
 
-  const div4 = document.createElement('div');
-  const input3 = document.createElement("INPUT");
+  var div4 = document.createElement('div');
+  var input3 = document.createElement("INPUT");
   input3.id = 'start-button';
   input3.setAttribute("type", "submit");
   input3.setAttribute("value", "Start Now");
@@ -182,15 +182,15 @@ $restart.addEventListener('click', (e) => {
   container.append(h1, span, h3, div1, div2, div3, div4);
   $main.append(container);
 
-  input3.addEventListener('click', (e) => {
-    const buttonArr = [];
+  input3.addEventListener('click', function(e) {
+    var buttonArr = [];
 
     buttonArr.push(button1);
     buttonArr.push(button2);
     buttonArr.push(button3);
     buttonArr.push(button4);
 
-    let xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple');
 
@@ -198,23 +198,23 @@ $restart.addEventListener('click', (e) => {
 
     counter--;
 
-    xhr.addEventListener('load', () => {
-      let xhrResponse = xhr.response;
-      let data = xhrResponse.results;
-      let answers = [];
-      let unfilteredCorrectAnswer = '';
+    xhr.addEventListener('load', function() {
+      var xhrResponse = xhr.response;
+      var data = xhrResponse.results;
+      var answers = [];
+      var unfilteredCorrectAnswer = '';
 
-      for (let i = 0; i < data.length; i++) {
+      for (var i = 0; i < data.length; i++) {
         h3.innerHTML = data[i].question;
         input3.value = 'Next';
         unfilteredCorrectAnswer = data[i].correct_answer;
-        const filteredCorrectAnswer = document.createElement('p');
+        var filteredCorrectAnswer = document.createElement('p');
         filteredCorrectAnswer.innerHTML = unfilteredCorrectAnswer;
-        const correctAnswer = filteredCorrectAnswer.textContent;
+        var correctAnswer = filteredCorrectAnswer.textContent;
         answers = (data[i].incorrect_answers);
         answers.push(correctAnswer);
         answers.sort();
-        buttonArr.forEach((button) => {
+        buttonArr.forEach(function(button) {
           button.classList.remove('hidden');
         })
         p.classList.remove('hidden');
@@ -223,16 +223,16 @@ $restart.addEventListener('click', (e) => {
         button3.innerHTML = answers[2];
         button4.innerHTML = answers[3];
 
-        e.target.addEventListener('click', (e) => {
-          buttonArr.forEach((button) => {
+        e.target.addEventListener('click', function(e) {
+          buttonArr.forEach(function(button) {
             if (button.textContent === correctAnswer) {
               button.className = 'green';
             }
           })
         })
 
-        buttonArr.forEach((button) => {
-          button.addEventListener("click", (e) => {
+        buttonArr.forEach(function(button) {
+          button.addEventListener("click", function(e) {
             if (e.target.textContent === correctAnswer) {
               scorer++;
               e.target.className = 'green';
@@ -248,14 +248,14 @@ $restart.addEventListener('click', (e) => {
         span.className = 'score';
         span.textContent = `Score: ${scorer}`;
         p.textContent = '';
-        buttonArr.forEach((button) => {
+        buttonArr.forEach(function(button) {
           button.className = 'gray';
         })
       }
 
       if (counter < 1) {
         h1.textContent = `Welldone, ${userData.username}!`;
-        buttonArr.forEach((button) => {
+        buttonArr.forEach(function(button) {
           button.classList.add('hidden');
         })
         input3.classList.add('hidden');
@@ -266,7 +266,7 @@ $restart.addEventListener('click', (e) => {
         } else if (scorer > 7) {
           h3.textContent = `We are in awe of your knowledge!`;
         }
-        const total = 10;
+        var total = 10;
         p.textContent = `You got ${scorer} of ${total} correctly.`;
         span.className = 'hidden';
         $main.append($restart);
